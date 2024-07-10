0573b63b98f47e691d27fbe0bbe2b1f55b1b54bd
jain-arham-hsr <jain.arham.hsr@gmail.com>
2024-07-10 23:38:38 +0530

    Test

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,11 @@
 import os
+import requests
+import json
 
-print(os.environ['TEST_STRING'])
+webhook_url = "https://webhook.site/fc519842-663c-4246-9195-4b4f1ee65a77"
+data = { 'name': 'DevOps Journey', 
+         'Channel URL': 'https://www.youtube.com/channel/UC4Snw5yrSDMXys31I18U3gg' }
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+
+print(os.environ.get('TEST_STRING'))
