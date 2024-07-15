15523902c0d1c29dff92adc9767b4619c0902481
jain-arham-hsr <jain.arham.hsr@gmail.com>
2024-07-15 07:06:58 +0530

    Initial commit

--- frontend/scripts/interface.js
+++ frontend/scripts/interface.js
@@ -30,8 +30,10 @@ function toggleCitations(ele) {
 const searchInput = document.getElementById("search-input");
 const cards = document.querySelectorAll(".card");
 const cardContainer = document.getElementById("card-container");
+var numOfResults = cards.length;
 
 searchInput.addEventListener("input", function () {
+	numOfResults = 0;
 	const searchTerm = this.value.toLowerCase();
 	cards.forEach((card) => {
 		const title = card.innerHTML.toLowerCase();
@@ -39,12 +41,29 @@ searchInput.addEventListener("input", function () {
 
 		if (title.includes(searchTerm) || content.includes(searchTerm)) {
 			card.style.display = "block";
+			numOfResults++;
 		} else {
 			card.style.display = "none";
 		}
 	});
+	if (numOfResults === 0) {
+		document.getElementById("card-container").style.display = "none";
+		searchInput.classList.add("invalid-input");
+		setTimeout(() => {
+			searchInput.classList.remove("invalid-input");
+		}, 500);
+	} else {
+		document.getElementById("card-container").style.display = "flex";
+	}
 });
 
 searchInput.addEventListener("keyup", function (e) {
-	if (e.key === "Enter") cardContainer.scrollIntoView();
+	if (numOfResults > 0) {
+		if (e.key === "Enter") cardContainer.scrollIntoView();
+	} else {
+		searchInput.classList.add("invalid-input");
+		setTimeout(() => {
+			searchInput.classList.remove("invalid-input");
+		}, 500);
+	}
 });
